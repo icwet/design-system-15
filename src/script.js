7d1144c3312392a2931331fcf6683b6cd0c0b469
Цветков Алексей Игоревич <ai_tsvetkov@bingo-boom.com>
2019-06-27 17:55:57 +0300

    Fix styles and bemjson layout

--- src/script.js
+++ src/script.js
@@ -12,126 +12,213 @@ const payment = bemEngine({
       elemMods: { size: 'm', align: 'center' },
       content: {
         block: 'grid',
-        mods: { 'm-columns': '12' },
-        content: {
-          block: 'payment',
-          mix: [{ block: 'form', mods: { border: 'all', view: 'default' } }],
-          content: [
-            {
-              block: 'payment',
-              elem: 'header',
-              mix: [
-                {
-                  block: 'form',
-                  elem: 'item',
-                  elemMods: { 'space-v': 'l', 'space-h': 'xl', border: 'bottom' }
-                }
-              ],
-              content: [
-                {
-                  block: 'text',
-                  mods: { view: 'primary', size: 'xxl' },
-                  content: {
-                    block: 'text',
-                    elem: 'word',
-                    elemMods: { width: 'l' }
-                  }
-                }
-              ]
+        mods: { 'm-columns': '10' },
+        content: [
+          {
+            block: 'grid',
+            elem: 'fraction',
+            elemMods: {
+              'm-col': '5'
             },
-            {
-              block: 'payment',
-              elem: 'content',
-              mix: [
-                {
-                  block: 'form',
-                  elem: 'item',
-                  elemMods: { 'space-v': 'xxxl', 'space-h': 'xl', border: 'bottom' }
-                }
-              ],
-              content: [
-                {
-                  block: 'form',
-                  elem: 'item',
-                  elemMods: {
-                    'indent-b': 'xl',
-                    distribute: 'between',
-                    'vertical-align': 'center'
-                  },
-                  content: [
-                    {
-                      block: 'text',
-                      mods: { view: 'primary', size: 'l' },
-                      content: {
-                        block: 'text',
-                        elem: 'word',
-                        elemMods: { width: 'l' }
+            content: [
+              {
+                block: 'payment',
+                mix: [{ block: 'form', mods: { border: 'all', view: 'default' } }],
+                content: [
+                  {
+                    block: 'payment',
+                    elem: 'header',
+                    mix: [
+                      {
+                        block: 'form',
+                        elem: 'item',
+                        elemMods: { 'space-v': 's', 'space-h': 'xl', border: 'bottom' }
                       }
-                    },
-                    {
-                      block: 'form',
-                      elem: 'label',
-                      elemMods: { width: 'default' }
-                    }
-                  ]
-                },
-                {
-                  block: 'form',
-                  elem: 'item',
-                  elemMods: {
-                    'indent-b': 'xl',
-                    distribute: 'between',
-                    'vertical-align': 'center'
-                  },
-                  content: [
-                    {
-                      block: 'text',
-                      mods: { view: 'primary', size: 'l' },
-                      content: {
+                    ],
+                    content: [
+                      {
                         block: 'text',
-                        elem: 'word',
-                        elemMods: { width: 'l' }
+                        mods: { view: 'primary', size: 'xxl', type: 'h1' },
+                        content: {
+                          block: 'text',
+                          elem: 'word',
+                          elemMods: { width: 'l' }
+                        }
                       }
-                    },
-                    {
-                      block: 'form',
-                      elem: 'label',
-                      elemMods: { width: 'default' }
-                    }
-                  ]
-                }
-              ]
-            },
-            {
-              block: 'payment',
-              elem: 'footer',
-              mix: [
-                {
-                  block: 'form',
-                  elem: 'item',
-                  elemMods: {
-                    distribute: 'between',
-                    border: 'bottom',
-                    'vertical-align': 'center',
-                    'space-v': 'l',
-                    'space-h': 'xl'
+                    ]
+                  },
+                  {
+                    block: 'payment',
+                    elem: 'content',
+                    mix: [
+                      {
+                        block: 'form',
+                        elem: 'item',
+                        elemMods: { 'space-v': 'xxxl', 'space-h': 'xl', border: 'bottom' }
+                      }
+                    ],
+                    content: [
+                      {
+                        block: 'form',
+                        elem: 'item',
+                        elemMods: {
+                          'indent-b': 'xl',
+                          distribute: 'between',
+                          'vertical-align': 'center'
+                        },
+                        content: [
+                          {
+                            block: 'form',
+                            elem: 'label',
+                            elemMods: { width: 'default' },
+                            content: [
+                              {
+                                block: 'text',
+                                mods: { view: 'primary', size: 'l' },
+                                content: {
+                                  block: 'text',
+                                  elem: 'word',
+                                  elemMods: { width: 'l' }
+                                }
+                              }
+                            ]
+                          },
+                          {
+                            block: 'form',
+                            elem: 'control',
+                            content: {
+                              block: 'input',
+                              mix: [{ block: 'payment', elem: 'input' }],
+                              mods: { size: 'l' }
+                            }
+                          }
+                        ]
+                      },
+                      {
+                        block: 'form',
+                        elem: 'item',
+                        elemMods: {
+                          'indent-b': 'xl',
+                          distribute: 'between',
+                          'vertical-align': 'center'
+                        },
+                        content: [
+                          {
+                            block: 'form',
+                            elem: 'label',
+                            elemMods: { width: 'default' },
+                            content: [
+                              {
+                                block: 'text',
+                                mods: { view: 'primary', size: 'l' },
+                                content: {
+                                  block: 'text',
+                                  elem: 'word',
+                                  elemMods: { width: 'l' }
+                                }
+                              }
+                            ]
+                          },
+                          {
+                            block: 'form',
+                            elem: 'control',
+                            content: {
+                              block: 'input',
+                              mix: [{ block: 'payment', elem: 'input' }],
+                              mods: { size: 'l' }
+                            }
+                          }
+                        ]
+                      }
+                    ]
+                  },
+                  {
+                    block: 'payment',
+                    elem: 'footer',
+                    mix: [
+                      {
+                        block: 'form',
+                        elem: 'item',
+                        elemMods: {
+                          distribute: 'between',
+                          border: 'bottom',
+                          'vertical-align': 'center',
+                          'space-v': 'l',
+                          'space-h': 'xl'
+                        }
+                      }
+                    ],
+                    content: [
+                      {
+                        block: 'text',
+                        mods: { view: 'primary', size: 'l' },
+                        content: {
+                          block: 'text',
+                          elem: 'word',
+                          elemMods: { width: 'l' }
+                        }
+                      },
+                      {
+                        block: 'button',
+                        mods: {
+                          size: 'l',
+                          width: 'full'
+                        }
+                      }
+                    ]
                   }
-                }
-              ],
-              content: [
-                {
-                  block: 'form',
-                  elem: 'item',
-                  elemMods: {
-                    'indent-b': 'xl',
-                    distribute: 'between',
-                    'vertical-align': 'center'
+                ]
+              }
+            ]
+          },
+          {
+            block: 'grid',
+            elem: 'fraction',
+            elemMods: { 'm-col': 5 },
+            content: [
+              {
+                block: 'warning',
+                mix: [
+                  { block: 'informer' },
+                  { block: 'theme', mods: { color: 'project-warning' } }
+                ],
+                content: [
+                  {
+                    block: 'warning',
+                    elem: 'content',
+                    mix: [
+                      {
+                        block: 'informer',
+                        elem: 'content',
+                        elemMods: { distribute: 'center', 'space-a': 'xxl' }
+                      }
+                    ],
+                    content: [
+                      {
+                        block: 'placeholder',
+                        mods: { size: 'm' }
+                      },
+                      {
+                        block: 'text'
+                      }
+                    ]
+                  },
+                  {
+                    block: 'warning',
+                    elem: 'button-wrapper',
+                    mix: [
+                      {
+                        block: 'informer',
+                        elem: 'action',
+                        elemMods: { distribute: 'center', 'space-a': 'xl' }
+                      }
+                    ]
                   }
-                }
-              ]
-            }
-          ]
-        }
+                ]
+              }
+            ]
+          }
+        ]
       }
     }
   }
