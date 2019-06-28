6ee10ac39e28743aa0b448be59551d22ecf741ed
Цветков Алексей Игоревич <ai_tsvetkov@bingo-boom.com>
2019-06-28 15:31:37 +0300

    add block history, add new images, new history styles into style.css

--- src/script.js
+++ src/script.js
@@ -326,7 +326,7 @@ const payment = bemEngine({
           {
             block: 'grid',
             elem: 'fraction',
-            elemMods: { 'm-col': '3' },
+            elemMods: { 'm-col': '2' },
             content: [
               {
                 block: 'product',
@@ -368,6 +368,330 @@ const payment = bemEngine({
                         elemMods: {
                           'space-a': 'm'
                         }
+                      }
+                    ],
+                    content: [
+                      {
+                        block: 'text',
+                        mods: { size: 'm', view: 'primary', type: 'p' },
+                        content: [
+                          {
+                            block: 'text',
+                            elem: 'word',
+                            elemMods: { width: 'l' }
+                          }
+                        ]
+                      },
+                      {
+                        block: 'text',
+                        mods: { size: 's', view: 'primary' },
+                        content: [
+                          {
+                            block: 'text',
+                            elem: 'word',
+                            elemMods: { width: 'm' }
+                          }
+                        ]
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            block: 'grid',
+            elem: 'fraction',
+            elemMods: { 'm-col': '5' },
+            content: [
+              {
+                block: 'history',
+                mix: [
+                  {
+                    block: 'list',
+                    mods: {
+                      view: 'default',
+                      border: 'all'
+                    }
+                  }
+                ],
+                content: [
+                  {
+                    block: 'history',
+                    elem: 'transaction',
+                    mix: [
+                      {
+                        block: 'list',
+                        elem: 'item',
+                        elemMods: {
+                          border: 'bottom',
+                          'space-a': 'l'
+                        }
+                      }
+                    ],
+                    content: [
+                      {
+                        block: 'history',
+                        elem: 'show',
+                        mix: [
+                          {
+                            block: 'e-accordion',
+                            elem: 'short'
+                          },
+                          {
+                            block: 'list',
+                            elem: 'item',
+                            elemMods: { 'vertical-align': 'center', 'indent-t': 'm' }
+                          }
+                        ],
+                        content: [
+                          {
+                            block: 'history',
+                            elem: 'details',
+                            content: [
+                              {
+                                block: 'history',
+                                elem: 'time',
+                                content: [
+                                  {
+                                    block: 'text',
+                                    mods: { view: 'ghost', size: 's' },
+                                    content: [
+                                      {
+                                        block: 'text',
+                                        elem: 'word',
+                                        elemMods: {
+                                          width: 'm'
+                                        }
+                                      }
+                                    ]
+                                  },
+                                  {
+                                    block: 'text',
+                                    mods: { view: 'ghost', size: 's' },
+                                    content: [
+                                      {
+                                        block: 'text',
+                                        elem: 'word',
+                                        elemMods: {
+                                          width: 'm'
+                                        }
+                                      }
+                                    ]
+                                  }
+                                ]
+                              },
+                              {
+                                block: 'history',
+                                elem: 'destination',
+                                mix: [
+                                  {
+                                    block: 'icon-plus',
+                                    mods: {
+                                      'vertical-align': 'center'
+                                    }
+                                  }
+                                ],
+                                content: [
+                                  {
+                                    block: 'history',
+                                    elem: 'pic',
+                                    mix: [
+                                      {
+                                        block: 'icon-plus',
+                                        elem: 'icon',
+                                        elemMods: {
+                                          'indent-r': 's',
+                                          'indent-l': 'l'
+                                        }
+                                      }
+                                    ],
+                                    content: [
+                                      {
+                                        block: 'brand-logo',
+                                        mods: {
+                                          name: 'yota',
+                                          size: 'm'
+                                        }
+                                      }
+                                    ]
+                                  },
+                                  {
+                                    block: 'history',
+                                    elem: 'label',
+                                    mix: [
+                                      {
+                                        block: 'icon-plus',
+                                        elem: 'block'
+                                      }
+                                    ],
+                                    content: [
+                                      {
+                                        block: 'text',
+                                        mods: {
+                                          view: 'primary',
+                                          size: 'l'
+                                        },
+                                        content: {
+                                          block: 'text',
+                                          elem: 'word',
+                                          elemMods: {
+                                            width: 'l'
+                                          }
+                                        }
+                                      },
+                                      {
+                                        block: 'text',
+                                        mods: {
+                                          view: 'primary',
+                                          size: 'l'
+                                        },
+                                        content: {
+                                          block: 'text',
+                                          elem: 'word',
+                                          elemMods: {
+                                            width: 'm'
+                                          }
+                                        }
+                                      }
+                                    ]
+                                  }
+                                ]
+                              }
+                            ]
+                          }
+                        ]
+                      },
+                      {
+                        block: 'history',
+                        elem: 'hide',
+                        mix: [
+                          {
+                            block: 'e-accordion',
+                            elem: 'more',
+                            elemMods: {
+                              view: 'default'
+                            }
+                          }
+                        ],
+                        content: [
+                          {
+                            block: 'list',
+                            elem: 'item',
+                            elemMods: {
+                              'indent-t': 'm'
+                            },
+                            content: [
+                              {
+                                block: 'history',
+                                elem: 'description',
+                                mix: [
+                                  {
+                                    block: 'list',
+                                    elem: 'item',
+                                    elemMods: {
+                                      'indent-b': 'm'
+                                    }
+                                  }
+                                ],
+                                content: [
+                                  {
+                                    block: 'text',
+                                    mods: {
+                                      size: 'l',
+                                      view: 'primary'
+                                    },
+                                    content: [
+                                      {
+                                        block: 'text',
+                                        elem: 'word',
+                                        elemMods: { width: 'l' }
+                                      },
+                                      {
+                                        block: 'text',
+                                        elem: 'word',
+                                        elemMods: { width: 'm' }
+                                      },
+                                      {
+                                        block: 'text',
+                                        elem: 'word',
+                                        elemMods: { width: 'l' }
+                                      }
+                                    ]
+                                  },
+                                  {
+                                    block: 'text',
+                                    mods: {
+                                      size: 'l',
+                                      view: 'primary'
+                                    },
+                                    content: [
+                                      {
+                                        block: 'text',
+                                        elem: 'word',
+                                        elemMods: { width: 'l' }
+                                      },
+                                      {
+                                        block: 'text',
+                                        elem: 'word',
+                                        elemMods: { width: 'm' }
+                                      },
+                                      {
+                                        block: 'text',
+                                        elem: 'word',
+                                        elemMods: { width: 'l' }
+                                      }
+                                    ]
+                                  }
+                                ]
+                              },
+                              {
+                                block: 'history',
+                                elem: 'actions',
+                                mix: [
+                                  {
+                                    block: 'list',
+                                    elem: 'item',
+                                    elemMods: {
+                                      distribute: 'between',
+                                      'indent-b': 'l'
+                                    }
+                                  }
+                                ],
+                                content: [
+                                  {
+                                    block: 'text',
+                                    mods: {
+                                      size: 'l',
+                                      view: 'primary'
+                                    },
+                                    content: [
+                                      {
+                                        block: 'text',
+                                        elem: 'word',
+                                        elemMods: { width: 'l' }
+                                      }
+                                    ]
+                                  },
+                                  {
+                                    block: 'text',
+                                    mods: {
+                                      size: 'l',
+                                      view: 'primary'
+                                    },
+                                    content: [
+                                      {
+                                        block: 'text',
+                                        elem: 'word',
+                                        elemMods: { width: 'm' }
+                                      }
+                                    ]
+                                  }
+                                ]
+                              }
+                            ]
+                          }
+                        ]
                       }
                     ]
                   }
