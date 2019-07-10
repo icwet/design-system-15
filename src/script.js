71db2284a18b6cb48020032f0ea3e6c1e9fd746b
Цветков Алексей Игоревич <ai_tsvetkov@bingo-boom.com>
2019-07-10 17:11:13 +0300

    fix script

--- src/script.js
+++ src/script.js
@@ -1,17 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-  /*const body = document.getElementsByTagName('body');
-  body.forEach(function(elem, i) {
-    const listElem = elem.querySelector('.history__transaction');
-    listElem.forEach(function(elem, i) {
-      elem.addEventListener('click', function(e) {
-        const showElem = elem.querySelector('history__show');
-        const hiddenElem = elem.querySelector('history__hide');
-        showElem.style.display = 'none';
-        hiddenElem.style.display = 'block';
-      });
-    });
-  });*/
+  const list = document.body.querySelector('.history');
+  const listElems = list.children;
+  for (let elem = 0; elem < listElems.length; elem++) {
+    listElems[elem].onclick = function(action) {};
+  }
 
-  const body = document.getElementsByTagName('body');
-  body.addEventListener();
+  function show(element) {
+    element.style.display = 'block';
+  }
+  function hide(element) {
+    element.style.display = 'none';
+  }
 });
