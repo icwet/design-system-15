905da9369acede72ea0dbf28ab39002589d13b19
Цветков Алексей Игоревич <ai_tsvetkov@bingo-boom.com>
2019-07-12 10:43:29 +0300

    complete accordion interactive

--- src/script.js
+++ src/script.js
@@ -1,21 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.body.addEventListener('click', function(event) {
     let target = event.target;
-    console.log(target);
 
-    const visibleElem = target.classList[0];
     while (target !== this) {
-      if (visibleElem === 'history__transaction') {
-        const hideElem = document.querySelector('.history__hide');
-        if (searchParent(hideElem, visibleElem)) {
-          hideElem.classList.toggle('e-accordion__more');
-        }
+      const visibleElemClass = target.classList[0];
+      if (visibleElemClass === 'history__transaction') {
+        target.childNodes[1].classList.toggle('e-accordion__more');
         return;
       }
       target = target.parentNode;
-    }
-    function searchParent(elem, elemParent) {
-      return Boolean(elem.closest(elemParent));
     }
   });
 });
