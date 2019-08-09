dc09ba90d0231721ce90fdae1e493962a2abd022
Цветков Алексей Игоревич <ai_tsvetkov@bingo-boom.com>
2019-08-09 11:37:52 +0300

    eslint fix

--- src/template-engine.js
+++ src/template-engine.js
@@ -9,7 +9,8 @@ export default function(obj) {
       obj.block = ctxBlock;
     }
 
-    let resultClass = obj.block || ctxBlock ? bemClass(obj) : '';
+    // eslint-disable-next-line no-use-before-define
+    const resultClass = obj.block || ctxBlock ? bemClass(obj) : '';
     if (resultClass === '') {
       return '';
     }
@@ -17,26 +18,28 @@ export default function(obj) {
     if (typeof obj !== 'object') {
       return obj;
     }
-    return ' class="' + resultClass + '"';
+    return ` class="${resultClass}"`;
   }
 
+  // eslint-disable-next-line no-shadow
   function bemClass(obj, argBlock) {
     const block = obj.block || argBlock;
     if (!block) {
       return '';
     }
-    const base = block + (obj.elem ? '__' + obj.elem : '');
+    const base = block + (obj.elem ? `__${obj.elem}` : '');
     const mods = obj.elem ? obj.elemMods : obj.mods;
 
     let output = base === argBlock ? '' : base;
 
     if (mods) {
-      for (let i in mods) {
+      // eslint-disable-next-line guard-for-in
+      for (const i in mods) {
         if (typeof mods[i] === 'number') {
           mods[i] += '';
         }
         if (mods[i]) {
-          output += ' ' + base + '_' + i + (mods[i] === true ? '' : '_' + mods[i]);
+          output += ` ${base}_${i}${mods[i] === true ? '' : `_${mods[i]}`}`;
         }
       }
     }
@@ -45,17 +48,17 @@ export default function(obj) {
       if (!Array.isArray(obj.mix)) {
         obj.mix = [obj.mix];
       }
-      for (let i = 0; i < obj.mix.length; i++) {
-        let mix = obj.mix[i];
-        if (!mix) {
-          continue;
+      for (let i = 0; i < obj.mix.length; i += 1) {
+        const mix = obj.mix[i];
+        if (mix) {
+          output += ` ${bemClass(mix, block)}`;
         }
-        output += ' ' + bemClass(mix, block);
       }
     }
     return output;
   }
 
+  // eslint-disable-next-line no-shadow
   function toHTML(obj, ctxBlock) {
     if (obj === undefined || obj === false || obj === null) {
       return '';
@@ -67,10 +70,11 @@ export default function(obj) {
       ctxBlock = obj.block;
     }
     if (Array.isArray(obj)) {
+      // eslint-disable-next-line no-use-before-define
       return concatArray(obj, ctxBlock);
     }
     if (!obj) {
-      return obj + '';
+      return `${obj}`;
     }
     if (obj.tag === false) {
       return toHTML(obj.content || '', ctxBlock);
@@ -79,13 +83,13 @@ export default function(obj) {
     const DEFAULT_TAG = 'div';
     obj.tag = obj.tag || DEFAULT_TAG;
 
-    const result = '<' + obj.tag + outputClasses(obj, ctxBlock);
-    return result + '>' + toHTML(obj.content, ctxBlock) + '</' + obj.tag + '>';
+    const result = `<${obj.tag}${outputClasses(obj, ctxBlock)}`;
+    return `${result}>${toHTML(obj.content, ctxBlock)}</${obj.tag}>`;
   }
 
   function concatArray(array, ctxBlock) {
     let output = '';
-    for (let i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length; i += 1) {
       if (array[i] !== undefined && array[i] !== false && array[i] !== null) {
         output += toHTML(array[i], ctxBlock);
       }
